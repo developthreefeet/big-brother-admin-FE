dd2283884ab8d02a65154706f091e459cae0f92a
xxxjinn <tlsguswls319@naver.com>
2024-07-23 20:26:12 +0900

    style: 학칙/회칙, 입/출금 내역/회의록 업로드 페이지 퍼블리싱

--- src/pages/proceeding/proceeding-upload/index.tsx
+++ src/pages/proceeding/proceeding-upload/index.tsx
@@ -1,5 +1,11 @@
+import UploadFile from '@/components/upload-file';
+
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <UploadFile title="회의록 업로드" />
+    </div>
+  );
 };
 
 export default index;
--- src/pages/transaction/transaction-upload/index.tsx
+++ src/pages/transaction/transaction-upload/index.tsx
@@ -1,5 +1,11 @@
+import UploadFile from '@/components/upload-file';
+
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <UploadFile title="입/출금 내역 업로드" />
+    </div>
+  );
 };
 
 export default index;
