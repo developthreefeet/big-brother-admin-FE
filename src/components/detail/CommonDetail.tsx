47bcde635d70db47f67f2aa2d3e226a60a4ac05d
xxxjinn <tlsguswls319@naver.com>
2024-07-24 02:04:52 +0900

    feature: 행사, faq, 공지사항 detail 페이지 퍼블리싱

---
+++ src/components/detail/CommonDetail.tsx
@@ -0,0 +1,27 @@
+import { Button } from 'antd';
+
+import { DataType } from '../list-table/types';
+
+function CommonDetail({ data }: { data: DataType }) {
+  return (
+    <div className="flex flex-col space-y-5 p-10">
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">{data.title}</h2>
+        <Button>수정하기</Button>
+      </div>
+      <div className="flex space-x-5">
+        <p>
+          <strong>게시일</strong> {data.upload_date}
+        </p>
+        <p>
+          <strong>수정일</strong> {data.edit_date}
+        </p>
+      </div>
+      <div className="rounded-sm border-2 border-gray-200 p-8">
+        <p>{data.content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CommonDetail;
--- src/pages/event/event-detail/index.tsx
+++ src/pages/event/event-detail/index.tsx
@@ -1,5 +1,18 @@
+import CommonDetail from '@/components/detail/CommonDetail';
+import { DataType } from '@/components/list-table/types';
+
 const index = () => {
-  return <div>detail 페이지</div>;
+  const data: DataType = {
+    key: '1',
+    id: '32',
+    title: '행사제목',
+    upload_date: '2024/07/23',
+    edit_date: '2024/07/24',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias eius consequuntur, autem illum adipisci saepe, dicta nobis maxime neque animi velit unde. Totam reprehenderit nobis ipsa consequatur! Quibusdam, quasi hic?',
+  };
+
+  return <CommonDetail data={data} />;
 };
 
 export default index;
