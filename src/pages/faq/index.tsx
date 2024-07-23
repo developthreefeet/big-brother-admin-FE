40916668d8758fce3e99bbd892b7b060b12a9089
xxxjinn <tlsguswls319@naver.com>
2024-07-23 17:09:53 +0900

    style: faq 목록 -> collapse에서 ListTable로 변경

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,54 +1,20 @@
-import ListCollapse from '@/components/collapse';
-import { ListCollapseProps } from '@/components/collapse/types';
+import ListTable from '@/components/list-table';
+import { DataType } from '@/components/list-table/types';
 
 function Faq() {
-  const data: ListCollapseProps['items'] = [
+  const data: DataType[] = [
     {
       key: '1',
-      label: 'Panel Header 1',
-      children: (
-        <div>
-          <p>Content for panel 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      ),
+      title: '질문1',
+      upload_date: '2024/07/23',
     },
     {
       key: '2',
-      label: 'Panel Header 2',
-      children: (
-        <div>
-          <p>Content for panel 2. Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.</p>
-        </div>
-      ),
-    },
-    {
-      key: '3',
-      label: 'Panel Header 3',
-      children: (
-        <div>
-          <p>Content for panel 3. Nulla facilisi. Aenean nec eros.</p>
-        </div>
-      ),
-    },
-    {
-      key: '4',
-      label: 'Panel Header 4',
-      children: (
-        <div>
-          <p>
-            Content for panel 4. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.
-          </p>
-        </div>
-      ),
+      title: '질문2',
+      upload_date: '2024/07/23',
     },
   ];
-
-  return (
-    <div className="flex flex-col space-y-5 pt-10">
-      <h1 className="mb-5 text-2xl font-bold">FAQ 목록</h1>
-      <ListCollapse items={data} />
-    </div>
-  );
+  return <ListTable data={data} route="/faq/upload" title="FAQ 목록" />;
 }
 
 export default Faq;
