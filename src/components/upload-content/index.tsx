419df0834b9c5668eec93c26cec6f5dfb1950f2c
xxxjinn <tlsguswls319@naver.com>
2024-08-04 01:08:55 +0900

    refactor: editor value state 변수명 변경

--- src/components/upload-content/index.tsx
+++ src/components/upload-content/index.tsx
@@ -4,13 +4,14 @@ import { useState } from 'react';
 import Editor from '../editor';
 
 function UploadContent({ title }: { title: string }) {
-  const [quillSimple, setQuillSimple] = useState('');
+  const [editorValue, setEditorValue] = useState('');
+
   return (
     <div className="flex flex-col space-y-5 pt-10">
       <h1 className="mb-5 text-2xl font-bold">{title}</h1>
       <div className="flex flex-col space-y-5">
         <Input placeholder="제목을 입력해주세요." />
-        <Editor sample value={quillSimple} onChange={setQuillSimple} />
+        <Editor sample value={editorValue} onChange={setEditorValue} />
       </div>
       <Button className="mx-auto w-52">작성 완료</Button>
     </div>
