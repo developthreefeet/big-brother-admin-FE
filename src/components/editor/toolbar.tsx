6d9e79b76565c7d0b145d21701cd9faebbc57ecc
xxxjinn <tlsguswls319@naver.com>
2024-08-04 01:08:04 +0900

    refactor: 툴바에서 불필요한 기능 제거하기

--- src/components/editor/toolbar.tsx
+++ src/components/editor/toolbar.tsx
@@ -89,19 +89,7 @@ export default function Toolbar({ id, isSimple, ...other }: EditorToolbarProps) 
         )}
 
         <div className="ql-formats">
-          <button type="button" className="ql-direction" value="rtl" />
-          <select className="ql-align" />
-        </div>
-
-        <div className="ql-formats">
-          <button type="button" className="ql-link" />
           <button type="button" className="ql-image" />
-          <button type="button" className="ql-video" />
-        </div>
-
-        <div className="ql-formats">
-          {!isSimple && <button type="button" className="ql-formula" />}
-          <button type="button" className="ql-clean" />
         </div>
       </div>
     </StyledToolbar>
