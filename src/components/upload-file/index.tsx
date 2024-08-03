0575093c5556d5fc1c7223e90107a28a63adeb10
xxxjinn <tlsguswls319@naver.com>
2024-08-04 02:01:19 +0900

    style: 파일 업로드 관련 text 수정

--- src/components/upload-file/index.tsx
+++ src/components/upload-file/index.tsx
@@ -1,8 +1,13 @@
-import { Button, Card, Input, message } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import { useState, useEffect } from 'react';
 
 import { Upload } from '@/components/upload';
 
 function UploadFile({ title }: { title: string }) {
+  const [form] = Form.useForm();
+  const [fileList, setFileList] = useState([]);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
   const beforeUpload = (file: File) => {
     const isPdf = file.type === 'application/pdf';
     if (!isPdf) {
@@ -11,14 +16,48 @@ function UploadFile({ title }: { title: string }) {
     return isPdf;
   };
 
+  const handleChange = ({ fileList }: any) => setFileList(fileList);
+
+  useEffect(() => {
+    const values = form.getFieldsValue();
+    setIsButtonDisabled(!(values.title && fileList.length > 0));
+  }, [fileList, form]);
+
   return (
     <div className="flex flex-col space-y-5 pt-10">
       <h1 className="mb-5 text-2xl font-bold">{title}</h1>
-      <Input placeholder="제목을 입력해주세요." />
-      <Card title="파일 업로드" className="w-full">
-        <Upload maxCount={1} name="single" beforeUpload={beforeUpload} />
-      </Card>
-      <Button className="mx-auto w-52">작성 완료</Button>
+      <Form
+        form={form}
+        onValuesChange={() => {
+          const values = form.getFieldsValue();
+          setIsButtonDisabled(!(values.title && fileList.length > 0));
+        }}
+      >
+        <Form.Item
+          name="title"
+          label="제목"
+          rules={[{ required: true, message: '제목을 입력해주세요.' }]}
+        >
+          <Input placeholder="제목을 입력해주세요." />
+        </Form.Item>
+        <Form.Item
+          label="파일 업로드"
+          rules={[{ required: true, message: '파일을 업로드해주세요.' }]}
+        >
+          <Upload
+            maxCount={1}
+            name="single"
+            beforeUpload={beforeUpload}
+            onChange={handleChange}
+            fileList={fileList}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" className="mx-auto w-52" disabled={isButtonDisabled}>
+            작성 완료
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
