fe4bc6b7db7253884f57579fe1bc6069bbebfe00
xxxjinn <tlsguswls319@naver.com>
2024-08-05 18:36:47 +0900

    feat: 업로드 페이지 (행사, 공지사항, faq) title 과 내용에 따른 버튼 활성화 로직 구현

--- src/components/upload-content/index.tsx
+++ src/components/upload-content/index.tsx
@@ -5,15 +5,32 @@ import Editor from '../editor';
 
 function UploadContent({ title }: { title: string }) {
   const [editorValue, setEditorValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const isButtonDisabled = inputValue.trim() === '' || editorValue.trim() === '';
+
+  const handleButtonClick = () => {
+    if (!isButtonDisabled) {
+      // upload api자리
+      console.log('Title:', inputValue);
+      console.log('Content:', editorValue);
+    }
+  };
 
   return (
     <div className="flex flex-col space-y-5 pt-10">
       <h1 className="mb-5 text-2xl font-bold">{title}</h1>
       <div className="flex flex-col space-y-5">
-        <Input placeholder="제목을 입력해주세요." />
-        <Editor sample value={editorValue} onChange={setEditorValue} />
+        <Input
+          placeholder="제목을 입력해주세요."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Editor value={editorValue} onChange={setEditorValue} />
       </div>
-      <Button className="mx-auto w-52">작성 완료</Button>
+      <Button className="mx-auto w-52" disabled={isButtonDisabled} onClick={handleButtonClick}>
+        작성 완료
+      </Button>
     </div>
   );
 }
