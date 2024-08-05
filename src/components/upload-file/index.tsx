3177e6ebd820b80b30e80915cd05825fbde1a6b3
HyunJin <102955516+xxxjinn@users.noreply.github.com>
2024-08-05 13:17:46 +0900

    pdf 업로드 컴포넌트 사용 페이지 로직 구현 (#23)
    * style: 파일 업로드 관련 text 수정
    
    * style: 업로드 컴포넌트 text 수정
    
    * feat: pdf 업로드 로직 구현

--- src/components/upload-file/index.tsx
+++ src/components/upload-file/index.tsx
@@ -1,8 +1,14 @@
-import { Button, Card, Input, message } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import { UploadFile } from 'antd/es/upload/interface';
+import { useState, useEffect } from 'react';
 
 import { Upload } from '@/components/upload';
 
-function UploadFile({ title }: { title: string }) {
+function UploadFileComponent({ title }: { title: string }) {
+  const [form] = Form.useForm();
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
   const beforeUpload = (file: File) => {
     const isPdf = file.type === 'application/pdf';
     if (!isPdf) {
@@ -11,16 +17,50 @@ function UploadFile({ title }: { title: string }) {
     return isPdf;
   };
 
+  const handleFileChange = ({ fileList }: { fileList: UploadFile[] }) => setFileList(fileList);
+
+  const handleSubmit = () => {
+    // 업로드 api자리
+    const values = form.getFieldsValue();
+    console.log('Title:', values.title);
+    console.log('File List:', fileList);
+  };
+
+  useEffect(() => {
+    const values = form.getFieldsValue();
+    setIsButtonDisabled(!(values.title && fileList.length > 0));
+  }, [fileList, form]);
+
   return (
-    <div className="flex flex-col space-y-5 pt-10">
+    <div className="flex flex-col pt-10">
       <h1 className="mb-5 text-2xl font-bold">{title}</h1>
-      <Input placeholder="제목을 입력해주세요." />
-      <Card title="파일 업로드" className="w-full">
-        <Upload maxCount={1} name="single" beforeUpload={beforeUpload} />
-      </Card>
-      <Button className="mx-auto w-52">작성 완료</Button>
+      <Form
+        form={form}
+        onValuesChange={() => {
+          const values = form.getFieldsValue();
+          setIsButtonDisabled(!(values.title && fileList.length > 0));
+        }}
+      >
+        <Form.Item name="title">
+          <Input placeholder="제목을 입력해주세요." />
+        </Form.Item>
+        <Form.Item name="file">
+          <Upload
+            maxCount={1}
+            name="single"
+            beforeUpload={beforeUpload}
+            onChange={handleFileChange}
+            fileList={fileList}
+          />
+        </Form.Item>
+      </Form>
+      <div className="flex justify-center">
+        <Button type="primary" className="w-52" disabled={isButtonDisabled} onClick={handleSubmit}>
+          작성 완료
+        </Button>
+      </div>
     </div>
   );
 }
 
-export default UploadFile;
+export default UploadFileComponent;
