17b7bf12ec0ba22b5086fd3da70fee345f1f0296
xxxjinn <tlsguswls319@naver.com>
2024-08-06 15:35:56 +0900

    feat: transaction mock data 수정

--- src/components/list-table/types.ts
+++ src/components/list-table/types.ts
@@ -1,3 +1,4 @@
+// 임시 (transaction 제외)
 export interface DataType {
   key: React.Key;
   id: string;
@@ -7,8 +8,19 @@ export interface DataType {
   edit_date?: string;
 }
 
+export interface TransactionDataType {
+  trans_id: number;
+  corporate_number: string;
+  bank_account_number: string;
+  trans_direction: string;
+  deposit: string;
+  withdraw: string;
+  balance: string;
+  trans_date: string;
+}
+
 export interface ListTableProps {
-  data: DataType[];
+  data: DataType[] | TransactionDataType[];
   title: string;
   route: string;
 }
--- src/pages/transaction/index.tsx
+++ src/pages/transaction/index.tsx
@@ -1,26 +1,41 @@
 import ListTable from '@/components/list-table';
-import { DataType } from '@/components/list-table/types';
+import { TransactionDataType } from '@/components/list-table/types';
+
+export const transactionData: TransactionDataType[] = [
+  {
+    trans_id: 1,
+    corporate_number: '1234567890',
+    bank_account_number: '9876543210',
+    trans_direction: 'deposit',
+    deposit: '5000',
+    withdraw: '0',
+    balance: '10000',
+    trans_date: '2024-08-06T10:00:00Z',
+  },
+  {
+    trans_id: 2,
+    corporate_number: '1234567890',
+    bank_account_number: '9876543210',
+    trans_direction: 'withdraw',
+    deposit: '0',
+    withdraw: '2000',
+    balance: '8000',
+    trans_date: '2024-08-06T11:00:00Z',
+  },
+  {
+    trans_id: 3,
+    corporate_number: '1234567890',
+    bank_account_number: '9876543210',
+    trans_direction: 'deposit',
+    deposit: '3000',
+    withdraw: '0',
+    balance: '11000',
+    trans_date: '2024-08-06T12:00:00Z',
+  },
+];
 
 function Transaction() {
-  const data: DataType[] = [
-    {
-      key: '1',
-      title: '입/출금 내역 제목',
-      upload_date: '2024/07/23',
-    },
-    {
-      key: '2',
-      title: '입/출금 내역 제목2',
-      upload_date: '2024/07/23',
-    },
-
-    {
-      key: '3',
-      title: '입/출금 내역 제목3',
-      upload_date: '2024/07/23',
-    },
-  ];
-  return <ListTable data={data} route="/transaction/upload" title="입/출금 내역 목록" />;
+  return <ListTable data={transactionData} route="/transaction/upload" title="입/출금 내역 목록" />;
 }
 
 export default Transaction;
