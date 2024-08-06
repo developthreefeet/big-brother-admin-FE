e48e4ef4e82c18c26c629e9abf35e0e91ebad7cb
xxxjinn <tlsguswls319@naver.com>
2024-08-06 14:31:00 +0900

    feat: 학칙 detail 페이지 로직 구현

--- src/pages/rule/index.tsx
+++ src/pages/rule/index.tsx
@@ -1,29 +1,30 @@
 import ListTable from '@/components/list-table';
 import { DataType } from '@/components/list-table/types';
 
-function Rule() {
-  const data: DataType[] = [
-    {
-      key: '1',
-      title: '학칙제목',
-      upload_date: '2024/07/23',
-      id: '122',
-    },
-    {
-      key: '2',
-      title: '학칙제목2',
-      upload_date: '2024/07/23',
-      id: '123',
-    },
+export const ruleData: DataType[] = [
+  {
+    key: '1',
+    title: '학칙제목',
+    upload_date: '2024/07/23',
+    id: '122',
+  },
+  {
+    key: '2',
+    title: '학칙제목2',
+    upload_date: '2024/07/23',
+    id: '123',
+  },
 
-    {
-      key: '3',
-      title: '학칙제목3',
-      upload_date: '2024/07/23',
-      id: '142',
-    },
-  ];
-  return <ListTable data={data} route="/rule/upload" title="학칙/회칙 목록" />;
+  {
+    key: '3',
+    title: '학칙제목3',
+    upload_date: '2024/07/23',
+    id: '142',
+  },
+];
+
+function Rule() {
+  return <ListTable data={ruleData} route="/rule/upload" title="학칙/회칙 목록" />;
 }
 
 export default Rule;
