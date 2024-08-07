59a1fc50c19a00f2755a59e8d0ce69a47b62ff78
xxxjinn <tlsguswls319@naver.com>
2024-08-07 23:54:30 +0900

    feat: 공지사항, faq, 행사 수정모드 추가

--- src/pages/faq/faq-detail/index.tsx
+++ src/pages/faq/faq-detail/index.tsx
@@ -1,18 +1,33 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import CommonDetail from '@/components/detail/CommonDetail';
+import UploadContent from '@/components/upload-content';
 import useDataMatch from '@/router/hooks/use-data-match';
+import editStore from '@/store/editStore';
 
 import { faqData } from '..';
 
 const index = () => {
   const data = useDataMatch(faqData);
+  const { isEditing, resetEditing } = editStore();
+  const location = useLocation();
+
+  useEffect(() => {
+    return () => resetEditing();
+  }, [location, resetEditing]);
 
   if (!data) {
     return null;
   }
 
-  return <CommonDetail data={data} />;
+  return (
+    <div>
+      {isEditing ? <UploadContent title="FAQ 수정" data={data} /> : <CommonDetail data={data} />}
+    </div>
+  );
 };
 
 export default index;
