7fcbfc29ba60ca127400d558003c3a169ba15059
xxxjinn <tlsguswls319@naver.com>
2024-10-05 22:41:11 +0900

    feat: 회의록 전체 조회 res, 회의록 content type 작성

--- src/components/list-table/types.ts
+++ src/components/list-table/types.ts
@@ -1,5 +1,6 @@
-// 임시 (transaction 제외)
-export interface DataType {
+// 임시 (transaction 제외 ) => 예전에 쓰던 코드
+
+export interface MockDataType {
   key: React.Key;
   id: string;
   title: string;
@@ -9,6 +10,11 @@ export interface DataType {
   public?: boolean;
   file?: string;
 }
+
+// 임시 => 예전에 쓰던 코드랑 합치기 위함
+export type DataType = MockDataType | ProceedingContent;
+
+// 여기서부터 실제로 필요한 타입들
 
 export interface TransactionDataType {
   trans_id: number;
@@ -26,4 +32,52 @@ export interface ListTableProps {
   data: DataType[];
   title: string;
   route: string;
+}
+
+interface ListFileInfo {
+  fileName: string;
+  url: string;
+  createAt: string;
+  updateAt: string;
+  id: number;
+  fileType: string;
+}
+
+export interface ProceedingContent {
+  createAt: string;
+  updateAt: string;
+  id: number;
+  title: string;
+  content: string;
+  affiliationId: number;
+  files?: ListFileInfo[];
+}
+
+export interface GetProceedingResData {
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  content: ProceedingContent[];
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  pageable: {
+    offset: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    paged: boolean;
+    pageNumber: number;
+    pageSize: number;
+    unpaged: boolean;
+  };
+  first: boolean;
+  last: boolean;
+  empty: boolean;
 }
