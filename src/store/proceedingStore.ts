e348a736ff01d9884a689f5c4b3ccea41ae68162
xxxjinn <tlsguswls319@naver.com>
2024-10-05 22:49:05 +0900

    feat: 전체 회의록 조회 infiniteQuery 생성

--- src/store/proceedingStore.ts
+++ src/store/proceedingStore.ts
@@ -1,20 +1,36 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export interface Proceeding {
-  key: string;
-  id: string;
-  title: string;
-  upload_date: string;
-  edit_date: string;
-  content: string;
-  file: string;
-}
+import contentService from '@/api/services/contentService';
+import { ProceedingContent } from '@/components/list-table/types';
+
+export const useGetProceedings = (affiliation: string) => {
+  return useInfiniteQuery({
+    queryKey: ['proceeding', affiliation],
+    queryFn: async ({ pageParam = 0 }) => {
+      const result = await contentService.getProceedings({
+        affiliation,
+        page: pageParam,
+        size: 7,
+        search: '',
+      });
+      return result;
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      if (lastPage.content.length < 7) return undefined;
+      return lastPage.number + 1;
+    },
+  });
+};
+
+/// 추후 없애야 함 (일단 전체 리스트 get만 구현해두었기 때문에)
 
 interface ProceedingState {
-  proceedings: Proceeding[];
-  addProceeding: (proceeding: Proceeding) => void;
-  updateProceeding: (id: string, updatedProceeding: Partial<Proceeding>) => void;
+  proceedings: ProceedingContent[];
+  addProceeding: (proceeding: ProceedingContent) => void;
+  updateProceeding: (id: number, updatedProceeding: Partial<ProceedingContent>) => void;
 }
 
 export const useProceedingStore = create<ProceedingState>()(
@@ -22,24 +38,20 @@ export const useProceedingStore = create<ProceedingState>()(
     (set) => ({
       proceedings: [
         {
-          key: '1',
-          id: '32',
+          id: 32,
           title: '8월 회의록',
-          upload_date: '2024-08-23',
-          edit_date: '2024-08-24',
+          createAt: '2024-08-23',
+          updateAt: '2024-08-24',
           content: '',
-          file: '/static/proceeding-1.pdf',
-          public: true,
+          affiliationId: 12,
         },
         {
-          key: '2',
-          id: '14',
+          id: 14,
           title: '7월 회의록',
-          upload_date: '2024-07-23',
-          edit_date: '2024-07-24',
+          createAt: '2024-07-23',
+          updateAt: '2024-07-24',
           content: '',
-          file: '/static/proceeding-1.pdf',
-          public: false,
+          affiliationId: 13,
         },
       ],
       addProceeding: (proceeding) =>
