6535b430e4a6c99b37e3cb2306e4540379679641
xxxjinn <tlsguswls319@naver.com>
2024-10-16 21:59:27 +0900

    refactor: ruleStore 내부 불필요한 zustand 관련 코드 삭제

--- src/store/ruleStore.ts
+++ src/store/ruleStore.ts
@@ -1,6 +1,4 @@
 import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query';
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 import contentService from '@/api/services/contentService';
 import { PostRes } from '@/api/types';
@@ -44,56 +42,3 @@ export const usePostRule = () => {
     },
   });
 };
-
-/// //////추후 삭제
-
-export interface Rule {
-  key: string;
-  id: string;
-  title: string;
-  upload_date: string;
-  edit_date: string;
-  content: string;
-  file: string;
-}
-
-interface RuleState {
-  rules: Rule[];
-  addRule: (rule: Rule) => void;
-  updateRule: (id: string, updatedRule: Partial<Rule>) => void;
-}
-
-export const useRuleStore = create<RuleState>()(
-  persist(
-    (set) => ({
-      rules: [
-        {
-          key: '1',
-          id: '32',
-          title: '2024 학칙',
-          upload_date: '2024-07-23',
-          edit_date: '2024-07-24',
-          content: '',
-          file: '/static/rule-1.pdf',
-        },
-        {
-          key: '2',
-          id: '14',
-          title: '2023 학칙',
-          upload_date: '2024-07-23',
-          edit_date: '2024-07-24',
-          content: '',
-          file: '',
-        },
-      ],
-      addRule: (rule) => set((state) => ({ rules: [...state.rules, rule] })),
-      updateRule: (id, updatedRule) =>
-        set((state) => ({
-          rules: state.rules.map((rule) => (rule.id === id ? { ...rule, ...updatedRule } : rule)),
-        })),
-    }),
-    {
-      name: 'rule-storage',
-    },
-  ),
-);
