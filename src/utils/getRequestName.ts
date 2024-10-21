5c4e80e055626fd008ec079642f26a96dc303f17
rhtjddls123 <jjjk0605@naver.com>
2024-10-21 16:26:05 +0900

    feat: getRequestName함수 분리

---
+++ src/utils/getRequestName.ts
@@ -0,0 +1,10 @@
+export const getRequestName = (pathname: string) => {
+  if (pathname.includes('notice')) return 'notice';
+  if (pathname.includes('event')) return 'event';
+  if (pathname.includes('faq')) return 'faq';
+  if (pathname.includes('rule')) return 'rule';
+  if (pathname.includes('proceeding')) return 'proceeding';
+  if (pathname.includes('transaction')) return 'transaction';
+  if (pathname.includes('user')) return 'user';
+  return '';
+};
